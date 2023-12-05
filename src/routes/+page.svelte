ec4237961758668575d72da2d46a8c20466d8242
mmilanovic <marko.milanovic@barrage.net>
2023-12-05 13:11:29 +0100

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { LightSwitch } from "@skeletonlabs/skeleton";
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<LightSwitch />
+</div>
+
